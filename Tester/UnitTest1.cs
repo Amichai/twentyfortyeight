8ee1bb510a90c87ff251121fbb4b3ee8b19b0cb0
amichai <amichaimlevy@gmail.com>
2014-06-20 18:31:36 -0400

    Initial commit.

---
+++ Tester/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwentyFortyEight;
+using System;
+
+namespace Tester {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestGetStateString() {
+            var inputString = "2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            var state = BoardState.Load(inputString);
+            var stateString = state.GetStateString();
+            Assert.AreEqual(inputString.Trim(','), stateString.Trim(','));
+        }
+
+        [TestMethod]
+        public void TestLeft() {
+            var inputString = "0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            var state = BoardState.Load(inputString);
+            state.Left();
+            var stateString = state.GetStateString();
+            var targetOutput = "2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            state = BoardState.Load(inputString);
+            state.Left();
+            stateString = state.GetStateString();
+            targetOutput = "4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            state = BoardState.Load(inputString);
+            state.Left();
+            stateString = state.GetStateString();
+            targetOutput = "4,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0";
+            state = BoardState.Load(inputString);
+            state.Left();
+            stateString = state.GetStateString();
+            targetOutput = "4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,64";
+            state = BoardState.Load(inputString);
+            state.Left();
+            stateString = state.GetStateString();
+            targetOutput = "4,2,0,0,0,0,0,0,0,0,0,0,64,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+        }
+
+        [TestMethod]
+        public void TestRight() {
+            var inputString = "0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            var state = BoardState.Load(inputString);
+            state.Right();
+            var stateString = state.GetStateString();
+            var targetOutput = "0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            state = BoardState.Load(inputString);
+            state.Right();
+            stateString = state.GetStateString();
+            targetOutput = "0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            state = BoardState.Load(inputString);
+            state.Right();
+            stateString = state.GetStateString();
+            targetOutput = "0,0,2,4,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0";
+            state = BoardState.Load(inputString);
+            state.Right();
+            stateString = state.GetStateString();
+            targetOutput = "0,0,4,4,0,0,0,0,0,0,0,0,0,0,0,0";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,64";
+            state = BoardState.Load(inputString);
+            state.Right();
+            stateString = state.GetStateString();
+            targetOutput = "0,0,2,4,0,0,0,0,0,0,0,0,0,0,0,64";
+            Assert.AreEqual(targetOutput, stateString);
+
+            inputString = "2,2,2,0,0,0,0,0,0,0,0,0,0,64,0,0";
+            state = BoardState.Load(inputString);
+            state.Right();
+            stateString = state.GetStateString();
+            targetOutput = "0,0,2,4,0,0,0,0,0,0,0,0,0,0,0,64";
+            Assert.AreEqual(targetOutput, stateString);
+        }
+    }
+}
---
+++ TwentyFortyEight/BoardState.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyFortyEight {
+    public class BoardState {
+        public BoardState() {
+            this.init();
+        }
+
+        public string GetStateString() {
+            string toReturn = string.Empty;
+            for (int i = 0; i < s * s; i++) {
+                var x = i % s;
+                var y = i / s;
+                toReturn += string.Format("{0},", this.state[x][y]);
+            }
+            return toReturn.Trim(',');
+        }
+
+        /// <summary>
+        /// "x,y,val"
+        /// </summary>
+        public static BoardState Load(string stateString) {
+            var toReturn = new BoardState();
+            var vals = stateString.Split(',').Select(i => i.Trim()).Select(i => int.Parse(i)).ToList();
+            for (int i = 0; i < vals.Count; i++) {
+                var x = i % s;
+                var y = i / s;
+                toReturn.state[x][y] = vals[i];
+            }
+            return toReturn;
+        }
+
+        private int rowCount(int rowIdx) {
+            int count = 0;
+            for (int i = 0; i < s; i++) {
+                if (this.state[i][rowIdx] != 0) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private int colCount(int colIdx) {
+            int count = 0;
+            for (int i = 0; i < s; i++) {
+                if (this.state[colIdx][i] != 0) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int[] this[int i] {
+            get {
+                return this.state[i];
+            }
+            set {
+                this.state[i] = value;
+            }
+        }
+
+        private bool press(ref int val1, ref int val2) {
+            if (val1 == 0 && val2 == 0) {
+                return false;
+            }
+            if (val1 == 0) {
+                val1 = val2;
+                val2 = 0;
+                return true;
+            }
+            if (val1 == val2) {
+                val1 = val1 + val2;
+                val2 = 0;
+                return false;
+            }
+            return false;
+        }
+
+        public void Left() {
+            for (int rowIdx = 0; rowIdx < s; rowIdx++) {
+                for (int x = 1; x < s; x++) {
+                    for (int j = x; j >= 1; j--) {
+                        var pressAgain = this.press(ref this[j - 1][rowIdx], ref this[j][rowIdx]);
+                        if (!pressAgain) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Right() {
+            for (int rowIdx = 0; rowIdx < s; rowIdx++) {
+                for (int x = s - 1; x >= 0; x--) {
+                    for (int j = x; j < s - 1; j++) {
+                        var pressAgain = this.press(ref this[j + 1][rowIdx], ref this[j][rowIdx]);
+                        if (!pressAgain) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Up() {
+            throw new Exception();
+        }
+
+        public void Down() {
+            throw new Exception();
+        }
+
+        private void init() {
+            this.state = new int[s][];
+            for (int i = 0; i < s; i++) {
+                this.state[i] = new int[s];
+            }
+        }
+
+        private const int s = 4;
+
+        private int[][] state;
+    }
+}
+
+
